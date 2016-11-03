d7ee3a2cee388124964ee05f44895b6849590bc2
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-11-02 23:07:14 -0400

    BoardConfig: Remove cm sepolicy; Add omni sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,8 +179,7 @@ BOARD_PROVIDES_LIBRIL := false
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/qcom/sepolicy.mk
+include vendor/omni/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/samsung/gprimelte-common/sepolicy
